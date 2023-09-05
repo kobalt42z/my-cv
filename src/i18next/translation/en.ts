b165efa632612f3b7a7ecd38162054f5f504fa3d
Shmouel kessous <chmouel2690@gmail.com>
2023-09-05 14:40:20 +0300

    fix

--- src/i18next/translation/en.ts
+++ src/i18next/translation/en.ts
@@ -7,26 +7,26 @@ export const en: ResourceLanguage = {
         "profession": "full stack developer",
         "statementTitle": "statment",
         "statementContent":
-            "A determined 20-year-old hailing from Marseille France, living in Jerusalem about 17 years, I've completed 12 years of schooling and chosen to serve in specialized tech units during my military service. With a strong dedication to this profession and a burning passion for technology, I've dedicated 1.5 years to intensive full-stack application development studies at Ort Syngalovski. I'm driven to learn and practice every day, recognizing that there's always more to explore and improve in the dynamic field of development.",
+            "I'm a 20-year-old from France, and I've been living in Jerusalem for about 17 years. I've completed 12 years of school, and I'm about to begin my service in specialized tech units. I'm truly committed to this profession, and I love technology. I've spent 1.5 years studying full-stack development at Ort Syngalovski, and I'm determined to keep learning and improving in this dynamic field.",
         "experianceTitle": "experience",
         "positionTitle1": "Private Fullstack Fundamentals Tutor",
         "positionContent1":
-            "I provided private tutoring services, specializing in teaching full-stack fundamentals. I worked with individuals who wanted to enhance their knowledge of web development at a medium level. My responsibilities included tailoring lesson plans to meet each student's unique learning style, ensuring a comprehensive understanding of core concepts such as HTML, CSS, JavaScript, and essential web development principles.",
-        "positionTitle2": "High School Web Fundamentals Instructor",
+            "I offered one-on-one tutoring, focusing on teaching the basics of full-stack web development. I helped people who wanted to improve their web development skills at a moderate level, making sure they had a solid grasp of key concepts like HTML, CSS, JavaScript, and essential web development principles.",
+        "positionTitle2": "High School Web Fundamentals Teacher",
         "positionContent2":
-            "I had the privilege of teaching web fundamentals to a group of 16-year-old boys at my former high school. I designed and delivered engaging lessons specifically designed for high school students, covering foundational topics like HTML, CSS, JavaScript, and introductory web development concepts. This opportunity allowed me to connect with and inspire young minds in a familiar environment, witnessing their growth and enthusiasm as they embarked on their coding journeys. It reinforced my belief in the transformative power of education.",
+            "I taught web development basics to 16-year-old students at my former high school, covering topics like HTML, CSS, JavaScript, and introductory web development concepts. This allowed me to connect with and educate young minds in a familiar setting.",
         "positionTitle3": "MADA Basic Life Support Hovesh",
         "positionContent3":
-            "I volunteered with MADA (Magen david Adom) as a Basic Life Support (Hovesh) responder, dedicating approximately 1000 hours of service. My responsibilities included providing life-saving assistance and care in ambulance settings and responding to various emergency situations at homes. This role involved numerous interventions during both daytime and nighttime hours, allowing me to gain valuable hands-on experience in critical medical response scenarios.",
+            "I volunteer with MADA (Magen David Adom) as a Basic Life Support (Hovesh) responder, dedicating about 1000 hours. I provide life-saving assistance in ambulances and respond to neighborhood emergencies, gaining valuable experience in critical medical responses",
         "educationTitle": "education",
         "educationTitle1": "Branco Weiss shaarey rahamim",
         "educationContent1a":
-            "I attended the Branco Weiss School, a unique institution in Israel for those transitioning from ultra-Orthodox backgrounds to a more academic environment. Here, I completed my high school education and earned the Bagrut diploma in just two years.",
+            "I attended the Branco Weiss School, an institution in Israel for individuals transitioning from ultra-Orthodox backgrounds. I completed my high school education and earned the Bagrut diploma in two years with 40 units.",
         "educationContent1b":
-            "Moreover, the Branco Weiss School places a strong emphasis on preparing its students for military service and developing their leadership skills. Through various activities and programs, I gained valuable insights and practical experiences that not only enriched my academic journey but also equipped me with the skills and confidence needed to excel in the armed forces and beyond.",
+            "Furthermore, the Branco Weiss School focuses on preparing students for military service and nurturing their leadership abilities. Through diverse activities and programs, I gained valuable insights and practical experiences that enriched my academic journey and equipped me with the skills and confidence for success in the armed forces and beyond.",
         "educationTitle2": "ORT Syngalovski",
         "educationContent2a":
-            "At ORT Syngalovski, I embarked on a transformative learning journey in the world of full-stack development. Over the course of one year, I delved deep into the MERN stack, mastering technologies such as JavaScript, TypeScript, React, Node.js, Express, Nest.js, MongoDB, SQL databases, and more. My dedication and self-driven learning allowed me to acquire a comprehensive skill set in this dynamic field.",
+            "I went to ORT Syngalovski and spent a year learning a lot about full-stack development. During that time, I became really good at using a bunch of technologies like JavaScript, TypeScript, React, Node.js, Express, Nest.js, MongoDB, SQL databases, and more.",
         "contactTitle": "contact",
         "skillsTitle": "Skills",
         "interestTitle": "interest",
